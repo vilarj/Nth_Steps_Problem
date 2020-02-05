31bfd963137a18b3f46e4bd9841bfab67b74b5d3
Vilarj <lamaravillasan@outlook.com>
2020-02-04 23:19:47 -0500

    Update Nth_Steps_Problem.py

--- Nth_Steps_Problem.py
+++ Nth_Steps_Problem.py
@@ -4,16 +4,16 @@ when each step is either one or two stages.
 
 (For example, a 3-stage ladder can be three ways: 1-1-1, 1-2, and 2-1)
 """
+class Nth_Problem:
+    n = input('Enter the number of steps: ')
+    result = Nth(n)
 
-n = input('Enter the number of steps: ')
-result = Nth(n)
+    print(f'Steps: {result}')
 
-print(f'Steps: {result}')
-
-def Nth(number):
-    if (number == 0 and number == 1):
-        return 1
-    if (number == 2):
-        return 2
-    else:
-        return Nth(number - 1) + Nth(number - 2) + Nth(number - 3)
+    def Nth(number):
+        if (number == 0 and number == 1):
+            return 1
+        if (number == 2):
+            return 2
+        else:
+            return Nth(number - 1) + Nth(number - 2) + Nth(number - 3)
